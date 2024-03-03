d29c43b9bc3ba787c373c4b6f712076863028a2a
urbw <urbw@AQUA-CB-URBW23.win.dtu.dk>
2024-03-03 19:42:54 +0100

    just a little change in the tutorial example

--- tutorials/importTutorial_codeOnly.m
+++ tutorials/importTutorial_codeOnly.m
@@ -1,44 +1,44 @@
 %% File import
 cd(fileparts(matlab.desktop.editor.getActiveFilename))
 clearvars
-dr=drEEMtoolbox;
+tbx=drEEMtoolbox;
 cd demofiles_AL_HYI\
-samples=dr.importeems('* - Waterfall Plot Sample.dat');
-blanks=dr.importeems('* - Waterfall Plot Blank.dat');
-absorbance=dr.importabsorbance('* - Abs Spectra Graphs.dat');
+samples=tbx.importeems('* - Waterfall Plot Sample.dat');
+blanks=tbx.importeems('* - Waterfall Plot Blank.dat');
+absorbance=tbx.importabsorbance('* - Abs Spectra Graphs.dat');
 cd ..
 
 %% Integration of samples, blanks, absorbance into one dataset
-[samples,blanks,absorbance]=dr.alignsamples(samples,blanks,absorbance);
+[samples,blanks,absorbance]=tbx.alignsamples(samples,blanks,absorbance);
 samples.absWave=absorbance.absWave;
 samples.abs=absorbance.abs;
-dr.validatedataset(samples);
-dr.addcomment(samples,"transferred absorbance to the sample EEM dataset")
-dr.addcomment(samples,"Just another comment here")
-dr.addcomment(samples,"And I had this other thought too")
+tbx.validatedataset(samples);
+tbx.addcomment(samples,"transferred absorbance to the sample EEM dataset")
+tbx.addcomment(samples,"Just another comment here")
+tbx.addcomment(samples,"And I had this other thought too")
 
 clearvars absorbance
 
 %% Metadata integration
 samples.filelist=erase(samples.filelist,{' (01)'});
-samples=dr.associatemetadata(samples,"metadata.xlsx",'sampleId');
-dr.addcomment(samples,"Not 100% of samples matched. Here, one can make a comment to tell myself to investigate this.")
-dr.addcomment(samples,"Now here, I really had something interesting to say")
+samples=tbx.associatemetadata(samples,"metadata.xlsx",'sampleId');
+tbx.addcomment(samples,"Not 100% of samples matched. Here, one can make a comment to tell myself to investigate this.")
+tbx.addcomment(samples,"Now here, I really had something interesting to say")
 
 
 %% All the processing before PARAFAC
-samples=dr.processabsorbance(samples);
-dr.addcomment(samples,"main issue addressed was the absorbance baseline offset")
+samples=tbx.processabsorbance(samples);
+tbx.addcomment(samples,"main issue addressed was the absorbance baseline offset")
 
 
-samples=dr.ifecorrection(samples);
-dr.addcomment(samples,"minor IFEs, but decided to correct.")
+samples=tbx.ifecorrection(samples);
+tbx.addcomment(samples,"minor IFEs, but decided to correct.")
 
-samples=dr.subtractblanks(samples,blanks);
-dr.addcomment(samples,"Blanks looked fine (no sign of fluorescence). Raman scatter almost gone.")
+samples=tbx.subtractblanks(samples,blanks);
+tbx.addcomment(samples,"Blanks looked fine (no sign of fluorescence). Raman scatter almost gone.")
 
-data=dr.ramancalibration(samples,blanks);
-dr.addcomment(data,"Calibration Raman scans looked good, integration parameters worked well.")
+data=tbx.ramancalibration(samples,blanks);
+tbx.addcomment(data,"Calibration Raman scans looked good, integration parameters worked well.")
 
 opt=handlescatterOptions; % or opt=handlescatter('options');
 opt.ray1 = [30 15];
@@ -53,88 +53,88 @@ opt.samples = 'all';
 opt.iopt =  "normal";
 opt.plot = false;
 opt.imethod = 'inpaint';
-data=dr.handlescatter(data,opt);
-dr.addcomment(data,"First try of cutting scatter. Seems to work ok.")
+data=tbx.handlescatter(data,opt);
+tbx.addcomment(data,"First try of cutting scatter. Seems to work ok.")
 
 
 
-data=dr.rmspikes(data);
-dr.vieweems(data)
-dr.explorevariability(data)
+data=tbx.rmspikes(data);
+tbx.vieweems(data)
+tbx.explorevariability(data)
 
 %% Dataset modifications prior to PARAFAC
-data=dr.subdataset(data,[],data.Em<310|data.Em>600,data.Ex>500);
-dr.addcomment(data,"Trimmed the edges of the EEM to focus on sensible areas")
+data=tbx.subdataset(data,[],data.Em<310|data.Em>600,data.Ex>500);
+tbx.addcomment(data,"Trimmed the edges of the EEM to focus on sensible areas")
 
 
-data=dr.scalesamples(data,1.7);
-dr.addcomment(data,"Scaling of EEMs since main factor is estuarine mixing.")
+data=tbx.scalesamples(data,1.7);
+tbx.addcomment(data,"Scaling of EEMs since main factor is estuarine mixing.")
 
-dr.displayhistory(data)
-dr.undolast(data)
+tbx.displayhistory(data)
+tbx.undolast(data)
 
-data=dr.scalesamples(data,1.5);
-dr.addcomment(data,"On second thought, I adjusted the scaling option slightly to have more drastic scaling.")
-
-
-data = dr.zapnoise(data,5,[350 370],275);
-dr.addcomment(data,"Demonstration of the zapnoise function")
+data=tbx.scalesamples(data,1.5);
+tbx.addcomment(data,"On second thought, I adjusted the scaling option slightly to have more drastic scaling.")
 
 
-data=dr.scalesamples(data,1.5);
-dr.addcomment(data,"On second thought, I adjusted the scaling option slightly")
+data = tbx.zapnoise(data,5,[350 370],275);
+tbx.addcomment(data,"Demonstration of the zapnoise function")
 
-data=dr.subdataset(data,1,[],[]);
-dr.addcomment(data,"Demonstration that deleting samples will also delete unscaled samples in the backup.")
+
+data=tbx.scalesamples(data,1.5);
+tbx.addcomment(data,"On second thought, I adjusted the scaling option slightly")
+
+data=tbx.subdataset(data,1,[],[]);
+tbx.addcomment(data,"Demonstration that deleting samples will also delete unscaled samples in the backup.")
 
 %% PARAFAC part
-data=dr.fitparafac(data,f=2:6, ...
+data=tbx.fitparafac(data,f=2:6, ...
     starts=2, ...
     convergence=1e-4,...
     parallelization=false, ...
     mode="overall");
-dr.addcomment(data,"Outliertest equivalent")
+tbx.addcomment(data,"Outliertest equivalent")
 
-dr.viewmodels(data)
+tbx.viewmodels(data)
 
-data=dr.subdataset(data,[],[],data.Ex<260);
-dr.addcomment(data,"Deleted short excitation due to noisyness.")
+data=tbx.subdataset(data,[],[],data.Ex<260);
+tbx.addcomment(data,"Deleted short excitation due to noisyness.")
 
-data=dr.fitparafac(data,f=2:6, ...
+data=tbx.fitparafac(data,f=2:6, ...
     starts=10, ...
     convergence=1e-6,...
     parallelization=false, ...
     mode="overall");
 
-dr.viewmodels(data)
-dr.addcomment(data,"Models loook fine for demo purpose.")
+tbx.viewmodels(data)
+tbx.addcomment(data,"Models loook fine for demo purpose.")
 
-data=dr.splitdataset(data);
-dr.addcomment(data,"First try at splitting. Let's see if this works.")
+data=tbx.splitdataset(data);
+tbx.addcomment(data,"First try at splitting. Let's see if this works.")
 
-data=dr.fitparafac(data,f=2:6, ...
+data=tbx.fitparafac(data,f=2:6, ...
     starts=4, ...
     convergence=1e-6,...
     parallelization=false, ...
     mode="split");
-dr.addcomment(data,"Models in all the splits (the overall model is there already).")
+tbx.addcomment(data,"Models in all the splits (the overall model is there already).")
 
-dr.viewmodels(data) % Validation results already visible here
+tbx.viewmodels(data) % Validation results already visible here
 
-data=dr.splitvalidation(data,5); % Just for visualization
-dr.addcomment(data,"Validation successful!")
+data=tbx.splitvalidation(data,5); % Just for visualization
+tbx.addcomment(data,"Validation successful!")
 
-data=dr.scalesamples(data,'reverse');
-dr.addcomment(data,"Reversed the scaling for export.")
+data=tbx.scalesamples(data,'reverse');
+tbx.addcomment(data,"Reversed the scaling for export.")
 
-dr.viewmodels(data)
-dr.viewhistory(data) % All backups still here
-dr.export2openfluor(data,2,'test2openfluor')
+tbx.viewmodels(data)
+tbx.viewhistory(data) % All backups still here
+tbx.export2openfluor(data,2,'test2openfluor')
 
 save("testSaving.mat","data") % This deletes backups
 load("testSaving.mat","data")
 
-dr.viewhistory(samples)  % No backups available
+tbx.viewhistory(samples)  % No backups available
 
 
 %% Experimental stuff
