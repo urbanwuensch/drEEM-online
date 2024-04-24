ca2c34c202317ffe7a29178ad5c192bc8f90fd38
urbw <urbw@AQUA-CB-URBW23>
2024-04-24 13:22:57 +0200

    remove backup functionality from drEEMhistory.

--- classes/drEEMhistory.m
+++ classes/drEEMhistory.m
@@ -9,7 +9,7 @@ classdef drEEMhistory
     % Hidden properties for toolbox-internal use only (not visible to user)
     properties (Hidden=true)
         details % <- Option structure or a text giving details.
-        backup (1,1)  % <- the dataout version at the end of call
+        backup (1,1)  % <- NOT IN USE!!! the dataout version at the end of call
         previous (1,1)  % <- for restoring the input variable of a call (currently only used for scaling)
     end
     methods (Static=true)
@@ -25,7 +25,7 @@ classdef drEEMhistory
             obj.timestamp=datetime("now");
             obj.fname=caller;
             obj.fmessage=message;
-            obj.details=details;
+            %obj.details=details;
             
             if backup.nSample~=0
                 backup=drEEMbackup.convert2backup(backup);
@@ -59,7 +59,7 @@ classdef drEEMhistory
         function tableout=convert2table(history)
             mustBeA(history,"drEEMhistory")
             flds={'timestamp','fname','fmessage','details','backup','previous','usercomment'};
-            
+
             conv=struct;
             for j=1:numel(flds)
                 for k=1:height(history)
@@ -70,4 +70,58 @@ classdef drEEMhistory
 
         end
     end
+
+    methods (Static=true,Hidden=true)
+        function [dataout] = restore(data,whichone)
+            arguments
+                data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
+                whichone (1,1) {mustBeNumeric}
+            end
+            try
+                mustBeLessThanOrEqual(whichone,numel(data.history))
+            catch
+                error(['2nd input must be less than or equal to ',num2str(numel(data.history)),'.'])
+            end
+
+            temp=data.history(whichone).backup;
+            temp=drEEMbackup.convert2dataset(temp);
+            temp.history=data.history(1:whichone);
+            temp.toolboxdata=data.toolboxdata;
+
+            if nargout==0
+                assignin("base",inputname(1),temp);
+                disp(['<strong> State ',num2str(whichone),' in "',inputname(1), '" restored. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
+                return
+            else
+                dataout=temp;
+            end
+
+        end
+
+        function [dataout] = undo(data)
+            arguments
+                data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
+            end
+            n=numel(data.history);
+            if n==1
+                error('Nothing to undo')
+            end
+
+            temp=data.history(n-1).backup;
+            temp=drEEMbackup.convert2dataset(temp);
+            temp.history=data.history(1:n-1);
+            temp.toolboxdata=data.toolboxdata;
+
+            if nargout==0
+                assignin("base",inputname(1),temp);
+                disp(['<strong> Last step (',num2str(n-1),') in dataset "',inputname(1), '" undone. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
+                return
+            else
+                dataout=temp;
+            end
+
+        end
+
+    end
+
 end
