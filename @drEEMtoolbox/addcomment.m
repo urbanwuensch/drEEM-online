7c20233dd2e09ed18aa0ceb430a045493ff17b15
urban <urban@MaMi.local>
2025-03-09 11:23:46 +0100

    add newline functionality

--- @drEEMtoolbox/addcomment.m
+++ @drEEMtoolbox/addcomment.m
@@ -1,4 +1,4 @@
-function dataout = addcomment(data,comment)
+function dataout = addcomment(data,comment,newopt)
 % <a href = "matlab:doc addcomment">dataout = addcomment(data,comment) (click to access documentation)</a>
 %
 % <strong>Add a comment to a drEEMdataset object</strong> to document your analysis process
@@ -13,6 +13,7 @@ function dataout = addcomment(data,comment)
 arguments
     data (1,1)    {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
     comment (1,:) {mustBeText}
+    newopt (1,:) {mustBeText,mustBeMember(newopt,["","newline"])} = string.empty
 end
 
 % Experimental feature; overwrite workspace variable, needs no outputarg check
@@ -22,19 +23,31 @@ end
 
 idx=height(data.history);
 dataout=data;
-if not(dataout.history(idx).usercomment=="")
-    % Solution 2: add a string
-    dataout.history(idx).usercomment=...
-        [dataout.history(idx).usercomment,string(comment)];
 
-else % New entry
-    dataout.history(idx).usercomment=string(comment);
-end
+if isempty(newopt)
+    % Add to existing entry
 
-if drEEMtoolbox.outputscenario(nargout)=="implicitOut"
-    assignin("base",inputname(1),dataout);
-    disp(['<strong> "',inputname(1), '" processed. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
-    return
+    if not(dataout.history(idx).usercomment=="")
+        % Solution 2: add a string
+        dataout.history(idx).usercomment=...
+            [dataout.history(idx).usercomment,string(comment)];
+
+    else % New entry
+        dataout.history(idx).usercomment=string(comment);
+    end
+
+    if drEEMtoolbox.outputscenario(nargout)=="implicitOut"
+        assignin("base",inputname(1),dataout);
+        disp(['<strong> "',inputname(1), '" processed. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
+        return
+    end
+
+else
+    % new drEEMhistory entry
+    idx=height(dataout.history)+1;
+    dataout.history(idx,1)=...
+        drEEMhistory.addEntry([char(mfilename),': user comment'],comment,[],dataout);
+    dataout.validate(dataout);
 end
 
 end
