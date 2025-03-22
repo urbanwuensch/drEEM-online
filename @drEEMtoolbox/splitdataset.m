2079afbfb18e360147d1d5468f6a56b0082b3f27
urban <urban@MaMi.local>
2025-03-22 16:12:43 +0100

    small improvements for the splitting operation

--- @drEEMtoolbox/splitdataset.m
+++ @drEEMtoolbox/splitdataset.m
@@ -72,12 +72,12 @@ if matches(splitType,'blind')
 end
 dataout=data;
 
-dataout.split=drEEMdataset; % Overwrite any preexisting split
+dataout.split=drEEMdataset.create; % Overwrite any preexisting split
 for j=1:options.numSplit
     out=not(splitIdent==j);
-    dataout.split(j,1)=drEEMdataset.rmsamples(data,out);
+    dataout.split(j,1)=cutsamples(data,out);
     dataout.split(j,1).history=...
-        drEEMhistory.addEntry(mfilename,'created dataset through splitting',options,drEEMdataset);
+        drEEMhistory.addEntry(mfilename,'created as a split of a larger dataset',options,drEEMdataset);
     dataout.split(j,1).models=drEEMmodel;
 end
 idx=height(dataout.history)+1;
@@ -117,4 +117,71 @@ function optValMC(splitType,option)
 if matches(splitType,'blind')&&not(isempty(option))
     warning('<strong>Conflicting inputs: </strong>splitType", takes precedent over specifying input to "metadataColumn".')
 end
+end
+
+function [dataout] = cutsamples(data,index)
+% Copyright (C) 2025 Urban J. Wuensch - wuensch@chalmers.se
+% Chalmers University of Technology
+% Department of Architecture and Civil Engineering
+% Sven Hultins Gata 6
+% 41296 Gothenburg (Sweden)
+arguments
+    data  (1,1) {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
+    index {mustBeNumericOrLogical}
+end
+
+if isempty(index)|sum(index)==0
+    %warning('rmsamples: input "index" was empty. No action taken.')
+    dataout=data;
+    return
+end
+
+drEEMdataset.validate(data)
+
+dataout=data;
+
+if islogical(index)|isnumeric(index)
+    dataout.X(index,:,:)=[];
+    dataout.i(index)=[];
+    dataout.filelist(index)=[];
+    dataout.metadata(index,:)=[];
+    if not(isempty(dataout.opticalMetadata))
+        dataout.opticalMetadata(index,:)=[];
+    end
+    if not(isempty(dataout.XBlank))
+        dataout.XBlank(index,:,:)=[];
+    end
+    if not(isempty(dataout.abs))
+        dataout.abs(index,:)=[];
+    end
+    dataout.nSample=size(dataout.X,1);
+else
+    error("input to index must be numeric or logical")
+end
+
+f=drEEMdataset.modelsWithContent(dataout);
+if not(isempty(f))
+    dataout.models=drEEMmodel;
+end
+
+
+
+if islogical(index)
+    index=find(index);
+end
+
+ident=data.filelist(index);
+if iscolumn(ident)
+    ident=ident';
+end
+if isscalar(ident)
+    ident=ident{1};
+else
+    newident=[];
+    for j=1:numel(ident)
+        newident=[newident,' ; ',ident{j}];
+    end
+    ident=newident;clearvars newident;
+end
+drEEMdataset.validate(dataout)
 end
