0c2ad82c763980d8cd1e9887558bb06a67602095
wuensch <wuensch@chalmers.se>
2026-02-18 13:45:36 +0100

    add saveAfterFunctionCall

--- @drEEMtoolbox/drEEMtoolbox.m
+++ @drEEMtoolbox/drEEMtoolbox.m
@@ -75,6 +75,38 @@ classdef drEEMtoolbox < handle
             out=vec==vec(idx);
 
         end
+        
+        function saveAfterFunctionCall(fig,name)
+            arguments
+                fig (1,1) {mustBeA(fig,'matlab.ui.Figure')}
+                name (1,1) {mustBeText}
+            end
+
+            name=char(name);
+            temp=strsplit(name,'.');
+            if isscalar(temp)
+                name=[name,'.png'];
+            elseif numel(temp)==2
+                ext=temp{end};
+                try
+                    mustBeMember(ext,["jpg","jpeg","png","tiff","tif","gif","svg","pdf","eps"])
+                catch ME
+                    message=replace(ME.message,'Value ','Extension input to "figurefile" ');
+                    ME = MException('MATLAB:validators:mustBeMember',message);
+                    throwAsCaller(ME)
+                end
+            elseif numel(temp)>2
+                warning('Export figure names should only contain one "." Replaced with "drEEM_figure.png"')
+                name='drEEM_figure.png';
+            end
+
+            exportgraphics(fig,name, ...
+                "BackgroundColor","white", ...
+                "ContentType","auto", ...
+                "Resolution",600, ...
+                "PreserveAspectRatio","on",...
+                "Padding","tight")
+        end
 
         f=dreemfig(fighandlein)
         f=dreemuifig
