f1a9e66f4ea48723ae256ebc1f5f82327f8baff2
wuensch <wuensch@chalmers.se>
2026-08-18 11:59:07 +0200

    remove unneeded methods (commented out)

--- classes/drEEMhistory.m
+++ classes/drEEMhistory.m
@@ -81,55 +81,55 @@ classdef drEEMhistory
     end
 
     methods (Static=true,Hidden=true)
-        function [dataout] = restore(data,whichone)
-            arguments
-                data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
-                whichone (1,1) {mustBeNumeric}
-            end
-            try
-                mustBeLessThanOrEqual(whichone,numel(data.history))
-            catch
-                error(['2nd input must be less than or equal to ',num2str(numel(data.history)),'.'])
-            end
+        % function [dataout] = restore(data,whichone)
+        %     arguments
+        %         data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
+        %         whichone (1,1) {mustBeNumeric}
+        %     end
+        %     try
+        %         mustBeLessThanOrEqual(whichone,numel(data.history))
+        %     catch
+        %         error(['2nd input must be less than or equal to ',num2str(numel(data.history)),'.'])
+        %     end
+        % 
+        %     temp=data.history(whichone).backup;
+        %     temp=drEEMbackup.convert2dataset(temp);
+        %     temp.history=data.history(1:whichone);
+        %     temp.toolboxdata=data.toolboxdata;
+        % 
+        %     if nargout==0
+        %         assignin("base",inputname(1),temp);
+        %         disp(['<strong> State ',num2str(whichone),' in "',inputname(1), '" restored. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
+        %         return
+        %     else
+        %         dataout=temp;
+        %     end
+        % 
+        % end
 
-            temp=data.history(whichone).backup;
-            temp=drEEMbackup.convert2dataset(temp);
-            temp.history=data.history(1:whichone);
-            temp.toolboxdata=data.toolboxdata;
-
-            if nargout==0
-                assignin("base",inputname(1),temp);
-                disp(['<strong> State ',num2str(whichone),' in "',inputname(1), '" restored. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
-                return
-            else
-                dataout=temp;
-            end
-
-        end
-
-        function [dataout] = undo(data)
-            arguments
-                data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
-            end
-            n=numel(data.history);
-            if n==1
-                error('Nothing to undo')
-            end
-
-            temp=data.history(n-1).backup;
-            temp=drEEMbackup.convert2dataset(temp);
-            temp.history=data.history(1:n-1);
-            temp.toolboxdata=data.toolboxdata;
-
-            if nargout==0
-                assignin("base",inputname(1),temp);
-                disp(['<strong> Last step (',num2str(n-1),') in dataset "',inputname(1), '" undone. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
-                return
-            else
-                dataout=temp;
-            end
-
-        end
+        % function [dataout] = undo(data)
+        %     arguments
+        %         data {mustBeA(data,"drEEMdataset"),drEEMdataset.validate(data)}
+        %     end
+        %     n=numel(data.history);
+        %     if n==1
+        %         error('Nothing to undo')
+        %     end
+        % 
+        %     temp=data.history(n-1).backup;
+        %     temp=drEEMbackup.convert2dataset(temp);
+        %     temp.history=data.history(1:n-1);
+        %     temp.toolboxdata=data.toolboxdata;
+        % 
+        %     if nargout==0
+        %         assignin("base",inputname(1),temp);
+        %         disp(['<strong> Last step (',num2str(n-1),') in dataset "',inputname(1), '" undone. </strong> Since no output argument was provided, the workspace variable was overwritten.'])
+        %         return
+        %     else
+        %         dataout=temp;
+        %     end
+        % 
+        % end
 
     end
 
